e2c30443593779ebd18460b0656be9901cbb3e72
Vu <tranngocv4220@gmail.com>
2026-06-28 23:31:08 +0700

    Deploy app with ArgoCD

--- deploy/istio/install_istio.sh
+++ deploy/istio/install_istio.sh
@@ -2,6 +2,4 @@ kubectl create namespace istio-system
 helm install istio-base istio/base -n istio-system
 helm install istiod istio/istiod -n istio-system -f deploy/istio/istiod-values.yaml
 
-kubectl create namespace istio-ingress
-helm install istio-ingress istio/gateway -n istio-ingress -f deploy/istio/istiod-values.yaml
-
+kubectl apply -k deploy/istio/addons
